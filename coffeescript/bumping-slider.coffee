a05805f9da1950b69f36f0ce936b70df3b5eba9a
Alessandro Vioni <jenoma@gmail.com>
2014-03-18 17:07:46 +0100

    Improvements

--- coffeescript/bumping-slider.coffee
+++ coffeescript/bumping-slider.coffee
@@ -23,23 +23,10 @@ $.fn.extend
 
       theWidht = $this.width()
 
-      theNumber = $(".container", $this).size()
-      thePercentage = 100 / theNumber
-      $(".container").css "width", "#{thePercentage}%"
-
-      setTimeout ->
-        theImageHeight = $("img").innerHeight()
-        $this.css
-          "height": theImageHeight
-          "opacity": 1
-      , 100
-
-      $(window).resize ->
-        theImageHeight = $("img").innerHeight()
-        $this.css
-          "height": theImageHeight
+      # theNumber = $(".container", $this).size()
+      # thePercentage = 100 / theNumber
+      # $(".container").css "width", "#{thePercentage}%"
 
       $(".container").each (index) ->
-        index ++
+        index +
         $(this).addClass "container-#{index}"
-
--- js/bumping-slider.js
+++ js/bumping-slider.js
@@ -16,30 +16,11 @@
         }
       };
       return this.each(function() {
-        var $this, theNumber, thePercentage, theWidht;
+        var $this, theWidht;
         $this = $(this);
         theWidht = $this.width();
-        theNumber = $(".container", $this).size();
-        thePercentage = 100 / theNumber;
-        $(".container").css("width", "" + thePercentage + "%");
-        setTimeout(function() {
-          var theImageHeight;
-          theImageHeight = $("img").innerHeight();
-          return $this.css({
-            "height": theImageHeight,
-            "opacity": 1
-          });
-        }, 100);
-        $(window).resize(function() {
-          var theImageHeight;
-          theImageHeight = $("img").innerHeight();
-          return $this.css({
-            "height": theImageHeight
-          });
-        });
         return $(".container").each(function(index) {
-          index + 1;
-          return $(this).addClass("container-" + index);
+          return index + $(this).addClass("container-" + index);
         });
       });
     }
